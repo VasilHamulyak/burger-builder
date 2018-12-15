b222beb5bdd7a637ff4d0f29b4ac95e55f1f8ef8
vasil <vasil.hamulyak@gmail.com>
2018-12-15 18:31:00 +0200

    feat: added handler error

--- src/containers/BurgerBuilder/BurgerBulider.js
+++ src/containers/BurgerBuilder/BurgerBulider.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErroHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -129,4 +130,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
